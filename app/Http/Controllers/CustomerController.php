4db6cd0a7092c8f0e386a36c943d21ce47a9d19d
kcchanuk <kccantab@gmail.com>
2026-03-19 18:02:56 +0000

    Update customer controller and request

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -5,35 +5,33 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CustomerRequest;
 use App\Models\Customer;
 use App\Http\Resources\CustomerResource;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 
 class CustomerController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the customers.
      *
-     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     * @return AnonymousResourceCollection
      */
-    public function index()
+    public function index(): AnonymousResourceCollection
     {
         return CustomerResource::collection(Customer::all());
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created customer in storage.
      *
      * @param CustomerRequest $request
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      */
-    public function store(CustomerRequest $request)
+    public function store(CustomerRequest $request): JsonResponse
     {
         // create customer first
-        $customer = Customer::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'utr' => $request->utr,
-            'dob' => $request->dob,
-            'phone' => $request->phone
-        ]);
+        $customer = Customer::create($request->validated());
 
         // handle profile pic file if it is in the request
         if ($request->hasFile('profile_pic')) {
@@ -45,34 +43,28 @@ class CustomerController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * Display the specified customer.
      *
      * @param Customer $customer
      * @return CustomerResource
      */
-    public function show(Customer $customer)
+    public function show(Customer $customer): CustomerResource
     {
         // Assume no related incomes are needed
         return new CustomerResource($customer);
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update the specified customer in storage.
      *
      * @param CustomerRequest $request
      * @param Customer $customer
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      */
-    public function update(CustomerRequest $request, Customer $customer)
+    public function update(CustomerRequest $request, Customer $customer): JsonResponse
     {
         // Update customer first
-        $customer->update([
-            'name' => $request->name,
-            'email' => $request->email,
-            'utr' => $request->utr,
-            'dob' => $request->dob,
-            'phone' => $request->phone
-        ]);
+        $customer->update($request->validated());
 
         // Assume if there is no new profile pic uploaded,
         // then there is no need to delete the original profile pic
@@ -83,22 +75,23 @@ class CustomerController extends Controller
             $customer->storeProfilePic($request->file('profile_pic'));
         }
 
-        return response()->json(new CustomerResource($customer), 200);
+        return response()->json(new CustomerResource($customer));
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified customer from storage.
      *
      * @param Customer $customer
-     * @return \Illuminate\Http\JsonResponse
+     * @return Response
      */
-    public function destroy(Customer $customer)
+    public function destroy(Customer $customer): Response
     {
-        $customer->incomes()->delete();
-        $customer->delete();
+        DB::transaction(function () use ($customer) {
+            $customer->incomes()->delete();
+            $customer->delete();
+        });
 
         // Assume there is no need to delete profile pics and income files
-
-        return response()->json(null, 204);
+        return response()->noContent();
     }
 }
--- app/Http/Requests/CustomerRequest.php
+++ app/Http/Requests/CustomerRequest.php
@@ -25,31 +25,31 @@ class CustomerRequest extends FormRequest
     public function rules()
     {
         $rules = [
-            'name' => 'bail|required|string|max:191',
-            'dob' => 'nullable|date_format:Y-m-d',
+            'name' => ['bail', 'required', 'string', 'max:191'],
+            'dob' => ['nullable', 'bail', 'string', 'date_format:Y-m-d'],
 
             // Assume profile pic file must be under 2MB
-            'profile_pic' => 'nullable|file|image|max:2048'
+            'profile_pic' => ['nullable', 'bail', 'file', 'image', 'max:2048']
         ];
 
         switch ($this->method()) {
             case 'POST':
-                $rules['email'] = 'bail|required|max:191|email|unique:customers,email';
+                $rules['email'] = ['bail', 'required', 'max:191', 'email', 'unique:customers,email'];
 
                 // UTR must be a 10-digit string
-                $rules['utr'] = ['bail', 'required', 'string', 'max:191', 'regex:/^[0-9]{10}$/',
-                    'unique:customers,utr'];
+                $rules['utr'] = ['bail', 'required', 'string', 'max:191', 'regex:/^[0-9]{10}$/', 'unique:customers,utr'];
 
                 // Assume to accept any format for phone.
                 // Usually phone country code, area code / phone number should be separate numeric input fields.
-                $rules['phone'] = 'nullable|string|max:191|unique:customers,phone';
+                $rules['phone'] = ['nullable', 'bail', 'string', 'max:191', 'unique:customers,phone'];
                 break;
 
             case 'PUT':
-                $rules['email'] = 'bail|required|max:191|email|unique:customers,email,' . $this->customer->id;
-                $rules['utr'] = ['bail', 'required', 'string', 'max:191', 'regex:/^[0-9]{10}$/',
-                    'unique:customers,utr,' . $this->customer->id];
-                $rules['phone'] = 'nullable|string|max:191|unique:customers,phone,' . $this->customer->id;
+                $rules['email'] = ['bail', 'required', 'max:191', 'email', 'unique:customers,email,' . $this->customer->id];
+
+                $rules['utr'] = ['bail', 'required', 'string', 'max:191', 'regex:/^[0-9]{10}$/', 'unique:customers,utr,' . $this->customer->id];
+
+                $rules['phone'] = ['nullable', 'bail', 'string', 'max:191', 'unique:customers,phone,' . $this->customer->id];
         }
 
         return $rules;
