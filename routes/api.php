df080a99ec8a999a66fb9657b2194782a32ac493
kcchanuk <kccantab@gmail.com>
2026-03-19 18:41:18 +0000

    Update route files

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\IncomeController;
 use App\Http\Controllers\ReportController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,12 +15,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-/*
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-*/
-
 Route::apiResource('customers', CustomerController::class);
 Route::apiResource('customers.incomes', IncomeController::class)->shallow();;
 Route::get('report', [ReportController::class, 'index'])->name('reports.index');
